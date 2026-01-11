e4846b8fa889e72a721ca4a39115c62e5f49b837
Checho0507 <checho.mlj@gmail.com>
2026-01-10 21:04:16 -0500

    6 mese capital

--- app/services/inversion.py
+++ app/services/inversion.py
@@ -41,7 +41,7 @@ def depositar_inversion(
     # Calcular fechas de retiro
     ahora = datetime.today(ZONE)
     proximo_retiro_intereses = ahora + timedelta(days=30)
-    proximo_retiro_capital = ahora
+    proximo_retiro_capital = ahora + timedelta(days=180)
     
     # Descontar del saldo del usuario
     usuario.saldo -= monto
