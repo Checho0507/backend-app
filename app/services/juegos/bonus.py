b6e07a65ef66853299b6d7c58b6eccb15015c923
Checho0507 <checho.mlj@gmail.com>
2026-01-18 21:10:30 -0500

    datetimetoday

--- app/services/juegos/bonus.py
+++ app/services/juegos/bonus.py
@@ -26,7 +26,8 @@ def reclamar_bonus_diario(
     if not user:
         raise HTTPException(status_code=404, detail="Usuario no encontrado")
 
-    hoy = datetime.today()-timedelta(hours=5)
+    hoy = datetime.today()
+    print(hoy)
 
     # Verificar si ya reclamó hoy
     if user.ultima_recompensa == hoy:
