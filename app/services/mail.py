7e2f8cc60d4fa54753cc3ae16ed2b612888adf1f
Checho0507 <checho.mlj@gmail.com>
2026-01-19 16:17:47 -0500

    mail no email

--- app/services/mail.py
+++ app/services/mail.py
@@ -1,11 +1,12 @@
 import smtplib
 from email.mime.text import MIMEText
 from email.mime.multipart import MIMEMultipart
+import os
 
-SMTP_SERVER = "smtp.gmail.com"
-SMTP_PORT = 587
-SMTP_USER = "tucorreo@gmail.com"
-SMTP_PASSWORD = "tu_contraseña_o_app_password"
+SMTP_SERVER = os.environ.get("SMTP_SERVER")
+SMTP_PORT = int(os.environ.get("SMTP_PORT", 587))
+SMTP_USER = os.environ.get("SMTP_USER")
+SMTP_PASSWORD = os.environ.get("SMTP_PASSWORD")
 
 
 def enviar_correo(destinatario: str, asunto: str, cuerpo: str):
